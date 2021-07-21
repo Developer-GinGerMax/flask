d0cbbeaed05bd0ebcbe3e9ea4b540db0e36b90c8
JS_KIM <86825671+Developer-GinGerMax@users.noreply.github.com>
2021-07-21 18:33:13 +0900

    end

--- scraping/music_ranking.py
+++ scraping/music_ranking.py
@@ -6,9 +6,9 @@ class MusicRangking(object):
     domain = ''
     query_string = ''
     html = ''
+    cname = ''
     headers = {'User-Agent': 'Mozilla/5.0'}
     class_name = []
-    cname = []
     artists = []
     titles = []
     dict = {}
@@ -22,25 +22,34 @@ class MusicRangking(object):
     def get_ranking(self):
         soup = BeautifulSoup(self.html, 'lxml')
         _ = 0
-        artists = soup.find_all(name=self.cname, attrs={'class': self.class_name})
-        titles = soup.find_all(name=self.cname, attrs={'class': self.class_name})
-        for i, j in zip(artists, titles):
+        artist = soup.find_all(name=self.cname, attrs={'class':self.class_name[0]})
+        title = soup.find_all(name=self.cname, attrs={'class':self.class_name[1]})
+        for i, j in zip(artist, title):
             _ += 1
             print(f"Rank {str(_)} Artist : {i.find('a').text}, Title : {j.find('a').text}")
+        self.class_name.clear()
 
     def insert_dict(self):
         '''
         # 방법 1
         for i in range(0, len(self.tag_name)):
             self.dict[self.titles[i]] = self.artists[i]
+        '''
+        soup = BeautifulSoup(self.html, 'lxml')
+        artist = soup.find_all(name=self.cname, attrs={'class': self.class_name[0]})
+        title = soup.find_all(name=self.cname, attrs={'class': self.class_name[1]})
+        for i, j in zip(artist, title):
+            self.artists.append(f"{i.find('a').text}")
+            self.titles.append(f"{j.find('a').text}")
+        self.class_name.clear()
+        print(self.titles)
         # 방법 2
         for i, j in zip(self.titles, self.artists):
             self.dict[i] = j
-        '''
         # 방법 3
-        for i, j in enumerate(self.titles):
-            self.dict[j] = self.artists[i]
 
+        # for i, j in enumerate(self.titles):
+        #     self.dict[j] = self.artists[i]
         print(dict)
 
     def dict_to_dataframe(self):
@@ -49,8 +58,8 @@ class MusicRangking(object):
 
     def df_to_csv(self):
         self.fname = input('file name : {}')
-        path = './data/{self.fname}.csv'
-        self.df.to_csv(path, sep=',', na_rep='NaN')
+        path = f'./data/{self.fname}.csv'
+        self.df.to_csv(path, sep='-', na_rep='NaN')
 
 def print_menu(ls):
     # return '\t'.join(ls)
@@ -74,25 +83,23 @@ def main():
             mr.set_html()
             mr.class_name.append('artist')
             mr.class_name.append('title')
-            mr.cname.append('p')
+            mr.cname = 'p'
         elif menu == 2:
             mr.domain = 'https://www.melon.com/chart/index.htm?dayTime='
             mr.query_string = '2021072016'
             mr.set_html()
             mr.class_name.append('ellipsis rank02')
             mr.class_name.append('ellipsis rank01')
-            mr.cname.append('div')
+            mr.cname = 'div'
         elif menu == 3:
             mr.get_ranking()
         elif menu == 4:
-            # mr.insert_dict()
+            mr.insert_dict()
             pass
         elif menu == 5:
-            # mr.dict_to_dataframe()
-            pass
+            mr.dict_to_dataframe()
         elif menu == 6:
-            # mr.df_to_csv()
-            pass
+            mr.df_to_csv()
         else:
             return
 
